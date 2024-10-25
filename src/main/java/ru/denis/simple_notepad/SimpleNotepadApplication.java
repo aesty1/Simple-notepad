e488bf5be59853cddfe6daefb3d6ada27b5c3433
aesty1 <kadirovdenis123@gmail.com>
2024-10-25 17:28:47 +0500

    first commit

---
+++ src/main/java/ru/denis/simple_notepad/SimpleNotepadApplication.java
@@ -0,0 +1,13 @@
+package ru.denis.simple_notepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNotepadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleNotepadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/denis/simple_notepad/SimpleNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.denis.simple_notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNotepadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
